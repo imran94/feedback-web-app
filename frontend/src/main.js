8a09b677b693c0f0aa8e6cd96ea24f5bfa90cafe
imran94 <iraja094@gmail.com>
2024-01-30 20:52:43 +0500

    Add classes in backend for notifications

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,22 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import PrimeVue from 'primevue/config'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import * as PusherPushNotifications from '@pusher/push-notifications-web'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+const beamsClient = new PusherPushNotifications.Client({
+  instanceId: 'f0fc087a-1695-4e40-8114-f78d417c68e2'
+})
+
+beamsClient
+  .start()
+  .then((beamsClient) => beamsClient.getDeviceId())
+  .then((deviceId) => console.log('Successfully registered with Beams. Device ID:', deviceId))
+  .catch(console.error)
 
 // app.config.globalProperties.$axios = axios
 app.use(createPinia())
